33d1f8e20bb59cb4863a16102127b6eae859bd0a
Soufiane Ghzal <sghzal@gmail.com>
2017-03-27 14:08:31 +0200

    update widgetDefinition docblock

--- src/javascripts/WidgetDefinition.js
+++ src/javascripts/WidgetDefinition.js
@@ -78,13 +78,22 @@ class WidgetDefinition {
  * @param {Array} [config.parentWhitelist] allowed parents. By default all parents are allowed.
  * @param {Array} [config.parentBlacklist] forbidden parents. By default all children no parent is forbidden.
  *
+ *
+ *
  * @param {boolean} [config.preview] array of callbacks that can be called during rendering
+ *
  * @param {boolean} [config.preview.onCreateContainer] called when the container dom element is created
  * but not populated yet. This method is called only when config.isContainer is set to true.
+ *
  * @param {boolean} [config.preview.afterCreateContainer] called after the container dom element is created
  * and populated. This method is called only when config.isContainer is set to true.
+ *
  * @param {boolean} [config.preview.onPreview] called when the preview dom element is created
  * but not populated yet, only when config.isContainer is false
+
+ * @param {boolean} [config.preview.onDataChange] called when the preview dom element is created
+ * but not populated yet, This method is called only when config.isContainer is set to false
+ *
  * @param {boolean} [config.preview.onCreateOuter] called when the outer div containing the element is created
  *
  * @returns {WidgetDefinition} createdWidget
