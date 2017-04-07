f934c9e8212b1e46bac224d240a46c46a342da5b
Soufiane Ghzal <sghzal@gmail.com>
2017-04-07 12:14:29 +0200

    remove console.log and push build

--- src/javascripts/Gui/WidgetPropertiesEditor/PropertyEditor.js
+++ src/javascripts/Gui/WidgetPropertiesEditor/PropertyEditor.js
@@ -24,7 +24,6 @@ class PropertyEditor{
     }
 
     afterDraw($editor, widget, property){
-        console.log($editor);
         // To be implemented by children that need logic after element is drawn
     }
 
